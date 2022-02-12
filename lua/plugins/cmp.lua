52393a6b384a3530e62a36f1401b333ffdcfbfbd
dangngo <ngominh.dang96@gmail.com>
2022-02-12 12:06:55 +0700

    fix(cmp): fix autocomplete not working

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,12 +7,12 @@ if not cmp_status_ok then
   return
 end
 
---local snip_status_ok, luasnip = pcall(require, "luasnip")
---if not snip_status_ok then
---  return
---end
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+  return
+end
 
---require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip/loaders/from_vscode").lazy_load()
 
 local check_backspace = function()
   local col = vim.fn.col "." - 1
@@ -51,13 +51,13 @@ local kind_icons = {
 
 cmp.setup {
   snippet = {
-    --expand = function(args)
-    --  luasnip.lsp_expand(args.body) -- For `luasnip` users.
-    --end,
+    expand = function(args)
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
+    end,
   },
   mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Tab>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -72,12 +72,12 @@ cmp.setup {
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      --elseif luasnip.expandable() then
-      --  luasnip.expand()
-      --elseif luasnip.expand_or_jumpable() then
-      --  luasnip.expand_or_jump()
-      elseif check_backspace() then
-        fallback()
+        --elseif luasnip.expandable() then
+        --  luasnip.expand()
+        --elseif luasnip.expand_or_jumpable() then
+        --  luasnip.expand_or_jump()
+        --elseif check_backspace() then
+        --  fallback()
       else
         fallback()
       end
@@ -88,8 +88,8 @@ cmp.setup {
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      --elseif luasnip.jumpable(-1) then
-      --  luasnip.jump(-1)
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
       else
         fallback()
       end
@@ -106,7 +106,7 @@ cmp.setup {
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
-        --luasnip = "[Snippet]",
+        luasnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
       })[entry.source.name]
@@ -131,4 +131,3 @@ cmp.setup {
     native_menu = false,
   },
 }
-
