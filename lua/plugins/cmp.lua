8b40396541dfd84a7720a9d7a1efe36729e25133
Dang Ngo <nmdang@fossil.com>
2022-04-11 23:04:03 +0700

    fix(nvim-cmp): fix luasnip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -12,7 +12,12 @@ if not snip_status_ok then
   return
 end
 
-require("luasnip/loaders/from_vscode").lazy_load()
+-- require("luasnip/loaders/from_vscode").lazy_load()
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 local check_backspace = function()
   local col = vim.fn.col "." - 1
@@ -72,12 +77,10 @@ cmp.setup {
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-        --elseif luasnip.expandable() then
-        --  luasnip.expand()
-        --elseif luasnip.expand_or_jumpable() then
-        --  luasnip.expand_or_jump()
-        --elseif check_backspace() then
-        --  fallback()
+       elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete() 
       else
         fallback()
       end
