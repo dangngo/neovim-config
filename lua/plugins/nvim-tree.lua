1c801f712fcbc53c18f0b513ae3a6ef76dfbefa4
dangngo <ngominh.dang96@gmail.com>
2022-05-28 22:03:59 +0700

    chore(nvim-tree): clean up config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,74 +2,54 @@
 -- Nvim Tree --
 ---------------
 
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
-
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
+-- local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+-- if not config_status_ok then
+--   return
+-- end
+--
+-- local tree_cb = nvim_tree_config.nvim_tree_callback
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-local keymaps = {
-  { key = {"<CR>"},                       action = "edit" },
-  { key = {"O"},                          action = "edit_no_picker" },
-  { key = {"<C-]>"},                      action = "cd" },
-  { key = "<C-v>",                        action = "vsplit" },
-  { key = "<C-x>",                        action = "split" },
-  { key = "<C-t>",                        action = "tabnew" },
-  { key = "<",                            action = "prev_sibling" },
-  { key = ">",                            action = "next_sibling" },
-  { key = "P",                            action = "parent_node" },
-  { key = "<BS>",                         action = "close_node" },
-  { key = "<Tab>",                        action = "preview" },
-  { key = "K",                            action = "first_sibling" },
-  { key = "J",                            action = "last_sibling" },
-  { key = "I",                            action = "toggle_ignored" },
-  { key = "H",                            action = "toggle_dotfiles" },
-  { key = "R",                            action = "refresh" },
-  { key = "a",                            action = "create" },
-  { key = "d",                            action = "remove" },
-  { key = "D",                            action = "trash" },
-  { key = "r",                            action = "rename" },
-  { key = "<C-r>",                        action = "full_rename" },
-  { key = "x",                            action = "cut" },
-  { key = "c",                            action = "copy" },
-  { key = "p",                            action = "paste" },
-  { key = "y",                            action = "copy_name" },
-  { key = "Y",                            action = "copy_path" },
-  { key = "gy",                           action = "copy_absolute_path" },
-  { key = "[c",                           action = "prev_git_item" },
-  { key = "]c",                           action = "next_git_item" },
-  { key = "-",                            action = "dir_up" },
-  { key = "s",                            action = "system_open" },
-  { key = "q",                            action = "close" },
-  { key = "g?",                           action = "toggle_help" },
-}
+-- Just for references
+-- local keymaps = {
+--   { key = {"<CR>"},                       action = "edit" },
+--   { key = {"O"},                          action = "edit_no_picker" },
+--   { key = {"<C-]>"},                      action = "cd" },
+--   { key = "<C-v>",                        action = "vsplit" },
+--   { key = "<C-x>",                        action = "split" },
+--   { key = "<C-t>",                        action = "tabnew" },
+--   { key = "<",                            action = "prev_sibling" },
+--   { key = ">",                            action = "next_sibling" },
+--   { key = "P",                            action = "parent_node" },
+--   { key = "<BS>",                         action = "close_node" },
+--   { key = "<Tab>",                        action = "preview" },
+--   { key = "K",                            action = "first_sibling" },
+--   { key = "J",                            action = "last_sibling" },
+--   { key = "I",                            action = "toggle_ignored" },
+--   { key = "H",                            action = "toggle_dotfiles" },
+--   { key = "R",                            action = "refresh" },
+--   { key = "a",                            action = "create" },
+--   { key = "d",                            action = "remove" },
+--   { key = "D",                            action = "trash" },
+--   { key = "r",                            action = "rename" },
+--   { key = "<C-r>",                        action = "full_rename" },
+--   { key = "x",                            action = "cut" },
+--   { key = "c",                            action = "copy" },
+--   { key = "p",                            action = "paste" },
+--   { key = "y",                            action = "copy_name" },
+--   { key = "Y",                            action = "copy_path" },
+--   { key = "gy",                           action = "copy_absolute_path" },
+--   { key = "[c",                           action = "prev_git_item" },
+--   { key = "]c",                           action = "next_git_item" },
+--   { key = "-",                            action = "dir_up" },
+--   { key = "s",                            action = "system_open" },
+--   { key = "q",                            action = "close" },
+--   { key = "g?",                           action = "toggle_help" },
+-- }
 
 nvim_tree.setup {
   auto_reload_on_write = true,
@@ -115,6 +95,26 @@ nvim_tree.setup {
     },
     icons = {
       webdev_colors = true,
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+        },
+      }
     },
   },
   hijack_directories = {
