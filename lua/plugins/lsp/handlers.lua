9fc8d28559ea75d1520a9b2ae791d42f43fa9ff5
dangngo <ngominh.dang96@gmail.com>
2022-05-29 15:16:44 +0700

    config(lsp): enable virtual text

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -14,7 +14,7 @@ M.setup = function()
 
   local config = {
     -- disable virtual text
-    virtual_text = false,
+    virtual_text = true,
     -- show signs
     signs = {
       active = signs,
