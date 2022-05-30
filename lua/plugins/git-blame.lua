fb92f84ca89cac87def70c4465ac07e06be36ed2
dangngo <ngominh.dang96@gmail.com>
2022-05-30 07:13:40 +0700

    config(git-blame): dont show in Neogit

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -1,7 +1,7 @@
 vim.g.gitblame_enabled = 0
 vim.g.gitblame_message_template = "<author> • <summary> • <date>"
 vim.g.gitblame_highlight_group = "Comment"
-vim.g.gitblame_ignored_filetypes = {"NvimTree", "toggleterm"}
+vim.g.gitblame_ignored_filetypes = {"NvimTree", "toggleterm", "Neogit"}
 vim.g.gitblame_set_extmark_options = {
   virt_text_pos = "right_align"
 }
