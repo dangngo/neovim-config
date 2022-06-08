7bbf9891b147e36cffb38037aded668af71dc9f6
dangngo <ngominh.dang96@gmail.com>
2022-06-08 20:31:47 +0700

    config(treesitter): only install needed languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,12 +8,45 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "all", -- one of "all" or a list of languages
+  ensure_installed = { -- one of "all" or a list of languages
+    "bash",
+    "cmake",
+    "comment",
+    "css",
+    "dockerfile",
+    "go",
+    "gomod",
+    "gowork",
+    "graphql",
+    "hcl",
+    "help",
+    "html",
+    "http",
+    "javascript",
+    "jsdoc",
+    "json",
+    "lua",
+    "make",
+    "proto",
+    "pug",
+    "python",
+    "regex",
+    "ruby",
+    "rust",
+    "scss",
+    "svelte",
+    "toml",
+    "tsx",
+    "typescript",
+    "vim",
+    "vue",
+    "yaml",
+  },
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { 
-    "norg",
-    "phpdoc"
-  }, -- List of parsers to ignore installing
+  -- ignore_install = { 
+  --   "norg",
+  --   "phpdoc"
+  -- }, -- List of parsers to ignore installing
   autopairs = { enable = true },
   indent = { 
     enable = true, 
