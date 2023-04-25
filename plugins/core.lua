f849f7358595079138f65972128c25fd3b9b7ae1
dangngo <ngominh.dang96@gmail.com>
2023-04-25 22:51:42 +0700

    neotree config

--- plugins/core.lua
+++ plugins/core.lua
@@ -39,6 +39,25 @@ return {
       source_selector = {
         winbar = false,
       },
+      filesystem = {
+        filtered_items = {
+          visible = true, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = true,
+          hide_gitignored = true,
+          hide_by_name = {
+            "node_modules",
+          },
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            ".DS_Store",
+            ".git",
+          },
+        },
+      },
+      window = {
+        mappings = {
+          ["u"] = "navigate_up",
+        },
+      },
     },
   },
   {
