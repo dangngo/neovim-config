c0dce67eed9ec761166ebaa2bc3362b1dada07aa
dangngo <ngominh.dang96@gmail.com>
2023-05-21 21:07:53 +0700

    update format on save

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
 return {
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
+    remote = "origin",     -- remote to use
+    channel = "stable",    -- "stable" or "nightly"
+    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly",    -- branch name (NIGHTLY ONLY)
+    commit = nil,          -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false,  -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
+    auto_quit = false,     -- automatically quit the current session after a successful update
   },
   colorscheme = "catppuccin",
   -- colorscheme = "nightfox",
@@ -20,14 +20,15 @@ return {
   lsp = {
     formatting = {
       format_on_save = {
-        enabled = false, -- enable or disable format on save globally
+        enabled = true,     -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           "go",
+          "terraform",
         },
         ignore_filetypes = {}, -- disable format on save for specified filetypes
       },
-      disabled = {}, -- disable formatting capabilities for the listed language servers
-      timeout_ms = 1000, -- default format timeout
+      disabled = {},           -- disable formatting capabilities for the listed language servers
+      timeout_ms = 1000,       -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
